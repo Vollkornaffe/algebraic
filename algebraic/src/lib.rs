9bec3578840d180f6821f1ad4468e6d372413272
Vollkornaffe <Vollkornaffe@gmail.com>
2022-11-22 22:05:49 +0100

    Add test utility

--- algebraic/src/lib.rs
+++ algebraic/src/lib.rs
@@ -12,11 +12,68 @@ generate_geometric_product!(6);
 #[cfg(test)]
 #[generic_tests::define]
 mod tests {
+    use rand_chacha::{rand_core::SeedableRng, ChaCha8Rng};
+    use rand_core::RngCore;
+
+    fn log_2(n: usize) -> usize {
+        assert!(n.is_power_of_two());
+        n.trailing_zeros() as usize
+    }
+
+    fn setup_rng() -> ChaCha8Rng {
+        rand_chacha::ChaCha8Rng::seed_from_u64(42)
+    }
+
+    fn random_float(rng: &mut ChaCha8Rng) -> f64 {
+        rng.next_u64() as f64 / std::u64::MAX as f64 * 20.0 - 10.0
+    }
+
+    fn random_vector<const N: usize>(rng: &mut ChaCha8Rng) -> [f64; N] {
+        let mut v = [0.0; N];
+        for i in (0..log_2(N)).map(|d| 1 << d) {
+            v[i] = random_float(rng);
+        }
+        v
+    }
+
+    fn random_multi_vector<const N: usize>(rng: &mut ChaCha8Rng) -> [f64; N] {
+        let mut mv = [0.0; N];
+        for i in 0..N {
+            mv[i] = random_float(rng);
+        }
+        mv
+    }
+
+    fn scale<const N: usize>(a: &[f64; N], s: f64) -> [f64; N] {
+        let mut b = a.clone();
+        for i in 0..N {
+            b[i] *= s;
+        }
+        b
+    }
+
+    fn add<const N: usize>(a: &[f64; N], b: &[f64; N]) -> [f64; N] {
+        let mut c = a.clone();
+        for i in 0..N {
+            c[i] += b[i];
+        }
+        c
+    }
+
+    fn sub<const N: usize>(a: &[f64; N], b: &[f64; N]) -> [f64; N] {
+        let mut c = a.clone();
+        for i in 0..N {
+            c[i] -= b[i];
+        }
+        c
+    }
+
+    fn approx<const N: usize>(a: &[f64; N], b: &[f64; N]) -> bool {
+        a.iter().zip(b.iter()).all(|(a, b)| (a - b).abs() < 0.0001)
+    }
 
     #[test]
-    fn antisymmetry<const N: usize>() {
-        println!("Testing for N = {}", N)
-    }
+    fn antisymmetry<const N: usize>() {}
 
     #[instantiate_tests(<1>)]
     mod d0 {}
