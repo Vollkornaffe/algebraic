bffa85eb9976e8742d17c17cfc00b5cddac3ddbb
Vollkornaffe <Vollkornaffe@gmail.com>
2022-11-22 21:51:52 +0100

    Setup generic testing

--- algebraic/src/lib.rs
+++ algebraic/src/lib.rs
@@ -8,3 +8,28 @@ generate_geometric_product!(3);
 generate_geometric_product!(4);
 generate_geometric_product!(5);
 generate_geometric_product!(6);
+
+#[cfg(test)]
+#[generic_tests::define]
+mod tests {
+
+    #[test]
+    fn antisymmetry<const N: usize>() {
+        println!("Testing for N = {}", N)
+    }
+
+    #[instantiate_tests(<1>)]
+    mod d0 {}
+    #[instantiate_tests(<2>)]
+    mod d1 {}
+    #[instantiate_tests(<4>)]
+    mod d2 {}
+    #[instantiate_tests(<8>)]
+    mod d3 {}
+    #[instantiate_tests(<16>)]
+    mod d4 {}
+    #[instantiate_tests(<32>)]
+    mod d5 {}
+    #[instantiate_tests(<64>)]
+    mod d6 {}
+}
