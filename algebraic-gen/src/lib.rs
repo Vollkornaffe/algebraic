982aba5084ae0bbd57af555ddc974a82e8e75f48
Vollkornaffe <Vollkornaffe@gmail.com>
2022-11-24 00:07:47 +0100

    Add documentation to the generated functions

--- algebraic-gen/src/lib.rs
+++ algebraic-gen/src/lib.rs
@@ -33,6 +33,30 @@ fn generate_product_string(product_sums: &[Vec<(bool, usize, usize)>]) -> String
     )
 }
 
+fn generate_base_string(elements: &[Vec<usize>]) -> String {
+    let dimension = elements.len().trailing_zeros() as usize;
+    elements.iter().enumerate().fold(
+        "\n\n|Index|Outer Product|Type|\n|-|-|-|".to_string(),
+        |bases, (i, element)| {
+            format!(
+                "{bases}\n|{}|{:?}|{}|",
+                i,
+                element,
+                match element.len() {
+                    0 => "Scalar".to_string(),
+                    k if k == dimension => "Pseudoscalar".to_string(),
+                    1 => "Vector".to_string(),
+                    2 => "Bivector".to_string(),
+                    3 => "Trivector".to_string(),
+                    4 => "Quadvector".to_string(),
+                    5 => "Quintvector".to_string(),
+                    k => format!("{}-Vector", k),
+                }
+            )
+        },
+    )
+}
+
 #[proc_macro]
 pub fn generate_geometric_product(input: TokenStream) -> TokenStream {
     let lit: syn::LitInt = syn::parse(input).unwrap();
@@ -52,10 +76,23 @@ pub fn generate_geometric_product(input: TokenStream) -> TokenStream {
     let product_stream = TokenStream::from_str(&product_string).unwrap();
     let product: ExprArray = syn::parse(product_stream).unwrap();
 
+    let basis = generate_base_string(&elements);
+    let documentation = format!(
+        "```{function_ident}``` calculates the geometric product of two multivectors (aka. Clifford
+        numbers or k-blades) from a geometric algebra of {}-dimensional space. The arguments and
+        return value are coefficient representations (```[T; {}]```) of the multivectors. ```T```
+        is meant to be a floating point type. The coefficients map to the ```2^{}``` orthogonal
+        basis elements which are listed below. The integers in the \'Outer Product\' column
+        represent different unit vectors and their wedge product. ",
+        dimension, array_length, dimension
+    );
+
     let gen = quote! {
-        pub fn #function_ident<E>(a: &[E; #array_length], b: &[E; #array_length]) -> [E; #array_length]
+        #[doc = #documentation]
+        #[doc = #basis]
+        pub fn #function_ident<T>(a: &[T; #array_length], b: &[T; #array_length]) -> [T; #array_length]
         where
-            E: Copy + Mul<E, Output = E> + Add<E, Output = E> + Sub<E, Output = E>,
+            T: Copy + Mul<T, Output = T> + Add<T, Output = T> + Sub<T, Output = T>,
         {
             #product
         }
